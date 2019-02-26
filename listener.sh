ecb2b5c0c9f5091a48bdda39e2e91252d911727d
SantriPasuruan <46401305+SantriPasuruan@users.noreply.github.com>
2019-02-26 08:41:13 +0000

    Create listener.sh

---
+++ listener.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+
+clear
+
+
+echo "
+
+<-- Indo -->
+
+####################
+
+# Tembak indosat #By Santri_Pasuruan
+
+# SANTRI TEAM OFFICIAL
+
+####################
+
+Semoga Berhasil";
+
+
+echo "";
+
+
+read -p "Server : http://" server
+
+
+read -p "Log File : " file
+
+
+read -p "Delay (2-5) : " delay
+
+
+echo "";
+
+
+echo "[+] Collecting Information...";
+
+
+sleep 3;
+
+
+echo "[+] Server : http://$server";
+
+
+sleep 1;
+
+
+echo "[+] File : $file";
+
+
+sleep 1;
+
+
+echo "[+] Delay : $delay seconds";
+
+
+sleep 2;
+
+
+echo "[+] Executing http://$server/$file...";
+
+
+sleep 3;
+
+
+while [ true ]
+
+
+do
+
+
+curl http://$server/$file
+
+
+sleep $delay
+
+
+clear
+
+
+done
+
